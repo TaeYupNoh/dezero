0c2e96f6f604a01973d958701a046615661c3e6d
yeop <shxoduq2@naver.com>
2023-02-26 00:48:10 +0900

    Add 02-06 code

--- step03.py
+++ step03.py
@@ -3,25 +3,31 @@ import numpy as np
 class Variable:
     def __init__(self, data):
         self.data = data
-
+        
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
         output = Variable(y)
         return output
-    
+        
     def forward(self, x):
         raise NotImplementedError()
 
 class Square(Function):
     def forward(self, x):
-        return x ** 2    
+        return x ** 2
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
     
-x = Variable(np.array(10))
-f = Square()
-y = f(x)
+A = Square()
+B = Exp()
+C = Square()
 
-print(type(y))
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
 print(y.data)
-
