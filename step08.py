e73290b3f1979a8ec20f8c844bfe1f3dfed83b36
yeop <shxoduq2@naver.com>
2023-02-26 17:46:59 +0900

    Add 06-10 code

--- step08.py
+++ step08.py
@@ -1,34 +1,50 @@
 import numpy as np
 
 class Variable:
-    def __init__(self, data):
+    def __init__(self,data):
         self.data = data
         self.grad = None
+        self.creator = None
         
+    def set_creator(self, func):
+        self.creator = func
+        
+    def backward(self):
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+            
+            if x.creator is not None:
+                funcs.append(x.creator)
+            
 class Function:
-    def __call__(self, input):
+    def __call__(self,input):
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        output.set_creator(self)
         self.input = input
+        self.output = output
         return output
     
     def forward(self, x):
         raise NotImplementedError()
-    
+
     def backward(self, x):
         raise NotImplementedError()
     
 class Square(Function):
     def forward(self, x):
         y = x ** 2
-        return y 
+        return y
     
     def backward(self, gy):
         x = self.input.data
         gx = 2 * x * gy
         return gx
-       
+    
 class Exp(Function):
     def forward(self, x):
         y = np.exp(x)
@@ -49,7 +65,5 @@ b = B(a)
 y = C(b)
 
 y.grad = np.array(1.0)
-b.grad = C.backward(y.grad)
-a.grad = B.backward(b.grad)
-x.grad = A.backward(a.grad)
+y.backward()
 print(x.grad)
