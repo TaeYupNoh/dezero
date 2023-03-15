57af8dd4f08d5abf3df50598d4535d42b6cef1b3
Taeyup <shxoduq2@naver.com>
2023-03-15 22:04:16 +0900

    Add 23-29 code

--- steps/step23.py
+++ steps/step23.py
@@ -1,5 +1,7 @@
+# 현재 파일이 위치한 디렉터리의 부모 디렉터리를 모듈 검색 경로에 추가
 if '__file__' in globals():
-    import os, sys
+    import os
+    import sys
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import numpy as np
