586d9fa465327ca00bac5a46d05e136f0bf62a9c
Taeyup <shxoduq2@naver.com>
2023-04-24 19:21:42 +0900

    Add 34, 35 code

---
+++ steps/step34.py
@@ -0,0 +1,31 @@
+if '__file__' in globals():
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+from dezero.utils import plot_dot_graph
+import matplotlib.pyplot as plt
+
+# 1차원 배열을 입력으로 활용
+x = Variable(np.linspace(-7, 7, 200))
+
+y = F.sin(x)
+y.backward(create_graph=True)
+
+# F.sin(x)의 data를 가져오기
+logs = [y.data]
+
+for i in range(3):
+    logs.append(x.grad.data)
+    gx = x.grad
+    x.cleargrad()
+    gx.backward(create_graph=True)
+
+labels = ["y=sin(x)", "y'", "y''", "y'''"]
+for i, v in enumerate(logs):
+    plt.plot(x.data, logs[i], label=labels[i])
+plt.legend(loc="lower right")
+plt.show()
