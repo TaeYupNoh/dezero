cd71fb29e46150c1fdb140306abe3bce886a60a2
Taeyup <shxoduq2@naver.com>
2023-05-02 16:49:57 +0900

    Revise 42 code

--- steps/step42.py
+++ steps/step42.py
@@ -15,20 +15,23 @@ x, y = Variable(x), Variable(y)
 W = Variable(np.zeros((1, 1)))
 b = Variable(np.zeros(1))
 
+
 def predict(x):
     y = F.matmul(x, W) + b
     return y
 
-def mean_squared_error(x0, x1):
+
+def mean_squared_error_simple(x0, x1):
     diff = x0 - x1
     return F.sum(diff ** 2) / len(diff)
+
 
 lr = 0.1
 iters = 100
 
 for i in range(iters):
     y_pred = predict(x)
-    loss = mean_squared_error(y, y_pred)
+    loss = F.mean_squared_error(y, y_pred)
 
     W.cleargrad()
     b.cleargrad()
